529bc62a0362cf59fc459518960e8fa1729ec2b9
yehia <anaa76746@gmail.com>
2024-10-10 02:51:36 +0300

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 //const uri = 'mongodb://localhost:27017/mydatabase'; //local MongoDB
 //for MongoDB Atlas
@@ -13,8 +14,11 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(err => console.error('MongoDB connection error:', err));
 
 const app = express();
+app.use(express.json());
+
 app.listen(3001, () => {
     console.log('Server is running on port 3001!');
 });
 
 app.use('/api/user' , userRoutes);
+app.use('/api/auth' , authRoutes);
