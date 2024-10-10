54fba37646a3386bc36956a84aa01fddfe8d0641
yehia <anaa76746@gmail.com>
2024-10-10 23:45:02 +0300

    dark mode

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -34,7 +34,7 @@ export default function SignIn() {
             }
         }
         catch(err) {
-            dispatch(signInFailure(err.message));
+            console.log(err.message);
         }
     };
     return (
