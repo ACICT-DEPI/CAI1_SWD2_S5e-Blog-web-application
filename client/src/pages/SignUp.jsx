7f7d91bb8f10478abaf10c41d2b0b362a90e5fb8
victory-17 <abdo17nasr@gmail.com>
2024-10-13 17:32:33 +0300

    update sign in & sign out

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,100 +1,118 @@
-import { useState } from "react";
-import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
-import {Link , useNavigate} from "react-router-dom";
-import OAuth from "../components/OAuth";
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
-    const [formData, setFormData] = useState({});
-    const [errorMessage , setErrorMessage] = useState(null);
-    const [loading , setLoading] = useState(false);
-    const navigate = useNavigate();
-    const handelChange = (e)=>{
-        setFormData({...formData, [e.target.id]: e.target.value.trim() });
-    };
-    const handelSubmit = async (e)=>{
-        e.preventDefault();
-        if(!formData.username || !formData.email || !formData.password){
-            return setErrorMessage("please fill out all fields. ");
-        }
-        try{ 
-            setLoading(true);
-            setErrorMessage(null);
-            const res = await fetch('/api/auth/signup',{
-                method: "POST",
-                headers: { 'Content-Type' : 'application/json' },
-                body: JSON.stringify(formData),
-            });
-            const data = await res.json();
-            if(data.success===false){
-                return setErrorMessage(data.message);
-            }
-            setLoading(false);
-            if(res.ok){
-                navigate('/sign-in');
-            }
-        }
-        catch(err) {
-            setErrorMessage(err.message);
-            setLoading(false);
-        }
-    };
-    return (
-        <div className="min-h-screen mt-20 ">
-            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-            <div className="flex-1">
-                {/* left side */}
-                <Link to="/" className="font-bold dark:text-white text-4xl">
-                <div className="flex items-center">
-                    <img src="../assets/logo.png" alt="Logo" />
-                    <span>iBlog</span>
-                </div>
-            </Link>
-            <p className="text-sm mt-5">
-                welcome to our blog website you can sign up by using your e-mail and password or by using google
-            </p>
-            </div>
-            {/* right side */}
-            <div className="flex-1">
-                    <form className=" flex flex-col gap-4" onSubmit={handelSubmit}>
-                        <div>
-                            <Label value="User Name"/>
-                            <TextInput type="text" placeholder="Username" id="username" onChange={handelChange} />
-                        </div>
-                        <div>
-                            <Label value="e-mail"/>
-                            <TextInput type="email" placeholder="name@company.com" id="email" onChange={handelChange} />
-                        </div>
-                        <div>
-                            <Label value="Password"/>
-                            <TextInput type="password" placeholder="Password" id="password" onChange={handelChange} />
-                        </div>
-                        <Button gradientDuoTone="purpleToPink" outline type="submit" disabled={loading}>
-                            {
-                                loading ? (
-                                    <>
-                                    <Spinner size="sm"/>
-                                    <span className="pl-3">
-                                        loading...
-                                    </span>
-                                    </>
-                                ) : 'Sign Up' 
-                            }
-                        </Button>
-                        <OAuth />
-                    </form>
-                    <div className=" flex gap-2 text-sm mt-5">
-                    <span> Have an account ? </span>
-                    <Link to='/sign-in' className="text-blue-500">
-                    Sign In
-                    </Link>
-                    </div>
-                    {
-                        errorMessage &&(<Alert className="mt-5" color={"failure"}>
-                            {errorMessage}
-                        </Alert>)
-                    }
-                </div>
-            </div>
+const [formData, setFormData] = useState({});
+const [errorMessage, setErrorMessage] = useState(null);
+const [loading, setLoading] = useState(false);
+const navigate = useNavigate();
+const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+};
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+    return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+    setLoading(true);
+    setErrorMessage(null);
+    const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    if (data.success === false) {
+        return setErrorMessage(data.message);
+    }
+    setLoading(false);
+    if(res.ok) {
+        navigate('/sign-in');
+    }
+    } catch (error) {
+    setErrorMessage(error.message);
+    setLoading(false);
+    }
+};
+return (
+    <div className='min-h-screen mt-20'>
+    <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+        <Link to='/' className='font-bold dark:text-white text-4xl'>
+            i
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+            Blog
+            </span>
+        </Link>
+        <p className='text-sm mt-5'>
+            This is a demo project. You can sign up with your email and password
+            or with Google.
+        </p>
         </div>
-    );   
+        {/* right */}
+
+        <div className='flex-1'>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+            <Label value='Your username' />
+            <TextInput
+                type='text'
+                placeholder='Username'
+                id='username'
+                onChange={handleChange}
+            />
+            </div>
+            <div>
+            <Label value='Your email' />
+            <TextInput
+                type='email'
+                placeholder='name@company.com'
+                id='email'
+                onChange={handleChange}
+            />
+            </div>
+            <div>
+            <Label value='Your password' />
+            <TextInput
+                type='password'
+                placeholder='Password'
+                id='password'
+                onChange={handleChange}
+            />
+            </div>
+            <Button
+            gradientDuoTone='purpleToPink'
+            type='submit'
+            disabled={loading}
+            >
+            {loading ? (
+                <>
+                <Spinner size='sm' />
+                <span className='pl-3'>Loading...</span>
+                </>
+            ) : (
+                'Sign Up'
+            )}
+            </Button>
+            <OAuth />
+        </form>
+        <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account?</span>
+            <Link to='/sign-in' className='text-blue-500'>
+            Sign In
+            </Link>
+        </div>
+        {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+            {errorMessage}
+            </Alert>
+        )}
+        </div>
+    </div>
+    </div>
+);
 }
